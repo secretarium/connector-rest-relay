69efca3f6f7976fdd590cfde26146ebdede77424
Florian Guitton <florian@secretarium.com>
2021-04-12 15:36:47 +0100

    feat: Ensuring key files can be loaded from .env

--- src/connector.ts
+++ src/connector.ts
@@ -1,11 +1,17 @@
 import { Key, SCP, Transaction } from '@secretarium/connector';
+import fs from 'fs';
 
-let currenKey;
+let currentKey;
 const currentConnection = new SCP();
 const connector = {
     initialize: async (): Promise<void> => {
-        currenKey = await Key.createKey();
-        return currentConnection.connect(process.env.SECRETARIUM_GATEWAY_ADDRESS ?? '', currenKey, process.env.SECRETARIUM_GATEWAY_TRUST_KEY ?? '');
+        const keyFile = process.env.SECRETARIUM_GATEWAY_PRIVATE_KEY_FILE;
+        if (keyFile && fs.existsSync(keyFile)) {
+            const encKey = JSON.parse(fs.readFileSync(keyFile).toString());
+            currentKey = await Key.importEncryptedKeyPair(encKey, process.env.SECRETARIUM_GATEWAY_PRIVATE_KEY_SECRET ?? '');
+        } else
+            currentKey = await Key.createKey();
+        return currentConnection.connect(process.env.SECRETARIUM_GATEWAY_ADDRESS ?? '', currentKey, process.env.SECRETARIUM_GATEWAY_TRUST_KEY ?? '');
     },
     request: async (dcApp: string, command: string, args: Record<string, unknown> | string, id: string): Promise<Transaction> =>
         currentConnection.newTx(dcApp, command, id, args)
